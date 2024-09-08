2662454c6fbc0b6a9588e8b11d33c399944efe30
AdarshReddy65 <adarshreddy65@gmail.com>
2024-09-08 18:51:36 +0530

    state and useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
+import { useState } from 'react';
 import './App.css';
 import Employee from './components/Employee';
 
 function App() {
-	console.log('we are about to list the employees!');
+	const [ role, setRole ] = useState('No Role!');
 	const showEmployees = true;
 	return (
 		<div className="App">
-			{console.log('inside the return')}
 			{showEmployees ? (
 				<>
+					<input type='text' onChange={(e)=>{
+						setRole(e.target.value)
+					}}/>
 					<Employee name="Adarsh" role="CEO"/>
-					<Employee name="Aditya"/>
+					<Employee name="Aditya" role={role}/>
 					<Employee name="Ashish"/>
 				</>
 			) : (
